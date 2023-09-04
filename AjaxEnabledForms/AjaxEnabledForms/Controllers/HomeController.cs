bc865573b049359fc47ba9bbe3af14b35ee4211f
s66829020@gmail.com <s66829020@gmail.com>
2023-09-04 22:14:02 +0530

    first commit

---
+++ AjaxEnabledForms/AjaxEnabledForms/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AjaxEnabledForms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxEnabledForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Student model)
+        {
+            StringBuilder sb = new StringBuilder("Form data:\n");
+            sb.Append(model.Name + ", " + model.Address + "," + model.Class + "," + model.Email);
+            return Content(sb.ToString());
+        }
+
+    }
+}
